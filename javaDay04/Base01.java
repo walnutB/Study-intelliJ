549edf3f1cdbf9bcbd7bcacf7bbc4ca347a8ef0e
walnutB <101103326+walnutB@users.noreply.github.com>
2023-04-13 18:20:46 +0900

    컴포넌트 오류 해결 및 브랜치 생성

---
+++ javaDay04/Base01.java
@@ -0,0 +1,18 @@
+package javaDay04;
+
+public class Base01 {
+    public static void main(String[] args) {
+        int [] a = new int [ 5 ];
+
+        a [ 0 ] = 10;
+        a [ 1 ] = 20;
+        a [ 2 ] = 30;
+        a [ 3 ] = 40;
+        a [ 4 ] = 50;
+
+        for ( int i = 0 ; i < 5 ; i++ ){
+            System.out.println( a [ i ] );
+        }
+
+    }
+}
