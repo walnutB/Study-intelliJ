9f614bc6c406d7894c69a28647ed2fe489a5ca40
walnutB <101103326+walnutB@users.noreply.github.com>
2023-04-13 13:21:31 +0900

    0318 - 복습

---
+++ JavaDay03/src/Base01.java
@@ -0,0 +1,12 @@
+public class Test01 {
+    public static void main(String[] args) {
+        for( int i = 0 ; i < 5 ; i++ ){
+            for( int j = 0 ; j < 5 ; j++ ){
+                if( i >= j ){
+                    System.out.print( " * " );
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ JavaDay03/src/Test01.java
@@ -0,0 +1,35 @@
+public class Test02 {
+    public static void main(String[] args) {
+//        다중 for문을 사용해서 아래 산 모양 출력하기
+//        *****
+//        ****
+//        ***
+//        **
+//        *
+        for ( int i = 1 ; i <= 5 ; i++){
+            for ( int j = 1 ; j <= 5 ; j++ ){
+                if ( i <= j ){
+                    System.out.print( "*" );
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for ( int i = 1 ; i <= 5 ; i++ ){
+            for ( int j = i ; j <= 5 ; j++ ){
+                System.out.print( "*" );
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for ( int i = 5 ; i > 0 ; i-- ){
+            for ( int j = 1 ; j <= i ; j++ ){
+                System.out.print( "*" );
+            }
+            System.out.println();
+        }
+
+    }
+}
